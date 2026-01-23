0ee92f233b63d05d339286fb3d6c247a7cb91d0e
ketakipatil57 <ketakip55@gmail.com>
2026-01-23 18:46:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version that includes classes , modules
+// this is by defualt applied 
+
+// alert("Hello")
+// node 01_basics/02_datatypes.js -> can;t use this syntax for executing alert
+
+// code should always be readable
+// Documentations : Mdn , tc93 ecma
+
+let name = "Ketaki" // string dtype
+let age = "20" // number 
+let isLoggedIn = false // boolean
+let state = null 
+
+
+// Primitive data types
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean = true, false =>used to check if user is logged in or not ,etc
+// null = standalone value=> null represents a null value/no value  (null is a object)
+// undefined => no value assigned
+// symbol => unique
+
+// Non-Primitive data types
+
+// object
+
+console.log(typeof "Ketaki")// => string
+console.log(typeof undefined) // => undefined
+console.log(typeof null) // => object
+
